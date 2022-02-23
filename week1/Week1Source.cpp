b8ab7e065ca75dfe3ea06ccf9a8198b3a5456ef7
FreaksMind <maroan0107@yahoo.com>
2022-02-23 22:09:25 +0200

    Fixat cateva erori

--- week1/Week1Source.cpp
+++ week1/Week1Source.cpp
@@ -7,10 +7,12 @@ int Div(int a, int b) { return a / b; }
 
 void ParseSources()
 {
-	char input[7] = "*O---";
+    char input[38] = "---H***E+++L+++L///O---P+++O/+-**O---";
 	func Operatori[4] = {Sum, Dif, Mul, Div};
-	int S, V;
-    Content x = { 0,0 };
+	int S = 0, V;
+    Content x;
+    x.p1 = 0;
+    x.p2 = 0;
 	int idx;
 
 	for (int i = 0; i < strlen(input); i++)
@@ -21,18 +23,22 @@ void ParseSources()
 				idx = 2;
 				x.p1 = 3;
 				x.p2 = 3;
+                break;
 			case SUMA:
 				idx = 0;
 				x.p1 = 7;
 				x.p2 = 5;
+                break;
 			case DIFERENTA:
 				idx = 1;
 				x.p1 = 10;
 				x.p2 = 1;
+                break;
 			case IMPARTIRE:
 				idx = 3;
 				x.p1 = 8;
 				x.p2 = 4;
+                break;
 		}
 
 		S = S + Operatori[idx](x.p1, x.p2);
